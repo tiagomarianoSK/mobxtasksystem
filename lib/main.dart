2d28771f3596793d4bc9bb24d37a30b8e9a325f8
tiagomarianoSK <marianobloggers@gmail.com>
2021-02-27 15:12:23 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:mobxtasksystem/screens/login_screen.dart';
+import 'package:mobxtasksystem/stores/login_store.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider<LoginStore>(
+      create: (_) => LoginStore(),
+      child: MaterialApp(
+        title: 'MobX Tutorial',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.deepPurpleAccent,
+          cursorColor: Colors.deepPurpleAccent,
+          scaffoldBackgroundColor: Colors.deepPurpleAccent,
+        ),
+        home: LoginScreen(),
+      ),
+    );
+  }
+}
